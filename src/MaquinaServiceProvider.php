e60342c17c7597fead3e28b24a7309ff03c92e6f
Fernando Sánchez <34519027+fsmonter@users.noreply.github.com>
2026-03-03 22:10:24 +0000

    Add package scaffolding with service provider, license, changelog, and editorconfig

---
+++ src/MaquinaServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Maquina;
+
+use Illuminate\Support\ServiceProvider;
+
+class MaquinaServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
